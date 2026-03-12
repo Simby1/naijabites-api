f8fbf9b5af9b9a589a32f84c27f876e73b859e2e
Simby1 <kimsimby@gmail.com>
2026-03-12 09:43:57 +0100

    feat: update authentication

--- core/settings.py
+++ core/settings.py
@@ -122,6 +122,10 @@ STATIC_URL = 'static/'
 
 # REST Framework settings
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from recipes.views import RecipeList, RecipeDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-token-auth/', obtain_auth_token),
     path('api/recipes/', RecipeList.as_view(), name='recipe-list'),
     path('api/recipes/<int:pk>/', RecipeDetailView.as_view(), name='recipe-detail'),
 ]
